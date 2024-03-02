48ebb712eca6b24a1241ee64022cb67072adf113
Murat KALE <kalemurat92@gmail.com>
2024-03-02 03:17:47 +0300

    bugfix(KM-1): route name changed to schedule

--- src/routes/scheduleRoute.ts
+++ src/routes/scheduleRoute.ts
@@ -1,18 +1,18 @@
 import express, { Request, Response } from 'express';
-import cronsController from '../controllers/cronsController';
+import scheduleController from '../controllers/scheduleController';
 // /crons
 const router = express.Router();
 
 router.get('/:id?', async (req: Request, res: Response) => {
-  return await cronsController.getCrons(req, res);
+  return await scheduleController.getCrons(req, res);
 });
 
 router.put('/create', async (req: Request, res: Response) => {
-  return await cronsController.createCron(req, res);
+  return await scheduleController.createCron(req, res);
 });
 
 router.put('/update/:id', async (req: Request, res: Response) => {
-  return await cronsController.updateCron(req, res);
+  return await scheduleController.updateCron(req, res);
 });
 
 export default router;
